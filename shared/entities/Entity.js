7826fea4e36ffe946464ec78f6010a393fd289a2
utrain <skassem05@outlook.com>
2023-01-03 23:54:06 -0500

    Added future notice

--- shared/entities/Entity.js
+++ shared/entities/Entity.js
@@ -1,3 +1,19 @@
+// Generally speaking, this is bad.
+// We should instead have components that can be attach to entities.
+/*
+	class SpaceCannon {
+		constructor(id) {
+			super(id)
+			this.units = new UnitComponent()
+		}
+	}
+*/
+
+// Unit component would then have stuff like...
+// .transferUnits
+// .moveUnitsOut
+// .getUnits
+
 module.exports = class Entity {
 	type = "unknown"
 	constructor(id) { this.id = id }
